3dedc6d4f810cc65cc38c006ded61c3fe78797c4
Hayden Ingino <hingino@gmail.com>
2025-12-02 23:08:30 -0700

    simple testing

--- app/web/src/routes/+page.svelte
+++ app/web/src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<center><h1>GardenGnome</h1></center>
+<div clas="container"><center>
+  <h1>GardenGnome</h1>
+  
+  <div class="row">
+    <div class="col">
+      <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    </div>
 
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<button class="btn btn-primary">Primary button</button>
+    <div class="col">
+      <button class="btn btn-primary">Primary button</button>
+    </div>
+  </div>
+</center></div>
